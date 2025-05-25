d98b105d31e3957693d8c2427ae9df53aaa783ff
Malte Bjoerklund <maltebjoerklund@gmail.com>
2025-05-25 23:45:21 +0200

    US31: Implementer filtrering af entities i eksport request (Done)

--- src/main/java/com/hovedopgave_dat_f25_backend/flight/FlightService.java
+++ src/main/java/com/hovedopgave_dat_f25_backend/flight/FlightService.java
@@ -1,5 +1,6 @@
 package com.hovedopgave_dat_f25_backend.flight;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,5 +25,49 @@ public class FlightService {
                     flight.getDepartureTime(),
                     flight.getArrivalTime()))
             .toList();
+    }
+
+    public List<FlightDTO> getFilteredFlights(List<JsonNode> filters) {
+        System.out.println("Filters: " + filters);
+        List<FlightDTO> flights = getAllFlights();
+
+        for (JsonNode filter : filters) {
+            JsonNode field = filter.get("flight").get("field");
+            JsonNode value = filter.get("flight").get("value");
+
+            if (field != null && value != null) {
+                String fieldStr = field.asText();
+                String valueStr = value.asText();
+                System.out.println("Field: " + fieldStr + ", Value: " + valueStr);
+
+                if (fieldStr.equals("flightNumber")) {
+                    flights = flights.stream()
+                            .filter(flight -> flight.flightNumber().equalsIgnoreCase(valueStr))
+                            .toList();
+                }
+                if (fieldStr.equals("departureTime")) {
+                    flights = flights.stream()
+                            .filter(flight -> flight.departureTime().equalsIgnoreCase(valueStr))
+                            .toList();
+                }
+                if (fieldStr.equals("arrivalTime")) {
+                    flights = flights.stream()
+                            .filter(flight -> flight.arrivalTime().equalsIgnoreCase(valueStr))
+                            .toList();
+                }
+                if (fieldStr.equals("airportOrigin")) {
+                    flights = flights.stream()
+                            .filter(flight -> String.valueOf(flight.airportOrigin().getId()).equalsIgnoreCase(valueStr))
+                            .toList();
+                }
+                if (fieldStr.equals("airportDestination")) {
+                    flights = flights.stream()
+                            .filter(flight -> String.valueOf(flight.airportDestination().getId()).equalsIgnoreCase(valueStr))
+                            .toList();
+                }
+            }
+        }
+
+        return flights;
     }
 }
